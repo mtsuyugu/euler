2361e444323c7cba106f75f9ca9061c93aa1f90c
Masaaki Tsuyuguchi <mtsuyugu@gmail.com>
2011-12-29 18:57:11 +0900

    refactor p10.scm

--- scm/p10.scm
+++ scm/p10.scm
@@ -1,5 +1,6 @@
 ;The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 ;Find the sum of all the primes below two million.
+(use srfi-1)
 
 (load "./eratosthenes.scm")
 
@@ -7,9 +8,9 @@
 (time (eratosthenes N))
 
 (define (p10)
-  (fold-with-index (lambda (idx x n)
-                     (if x (+ n idx) n))
-                   0
-                   prime-table))
+  (fold (lambda (x n)
+          (if (prime? x) (+ n x) n))
+        0
+        (iota N 1)))
 
 (print (p10))

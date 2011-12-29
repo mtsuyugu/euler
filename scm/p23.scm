c90fc14f0544343e1ead54c94525f61162fc42fc
Masaaki Tsuyuguchi <mtsuyugu@gmail.com>
2011-12-29 18:57:55 +0900

    solved p23.scm

--- scm/p23.scm
+++ scm/p23.scm
@@ -1,70 +1,31 @@
-(use util.stream)
+(use srfi-1)
+(use gauche.sequence)
+(load "./util.scm")
 
-(define divisible? (lambda (n d) (= (modulo n d) 0)))
-(define square (lambda (x) (* x x)))
+(define (abundant? x)
+  (> (fold + 0 (divisors x)) x))
 
-(define (integers-starting-from n)
-    (stream-cons n (integers-starting-from (+ n 1))))
+(define (p23)
+  (let* ((limit 28123)
+         (table (make-vector limit #t)))
+    (let loop ((i 12) (abundant-list '()))
+      (if (>= i limit)
+        (fold-with-index (lambda (idx n r)
+                           (if (vector-ref table idx)
+                             (+ idx r)
+                             r))
+                         0 table)
+        (let* ((isabundant (abundant? i))
+               (abundant-list (if isabundant 
+                                (cons i abundant-list)
+                                abundant-list)))
+          (if isabundant
+            (for-each (lambda (ab)
+                        (if (< (+ ab i) limit)
+                          (vector-set! table (+ ab i) #f)))
+                      abundant-list))
+          (loop (+ i 1) abundant-list))))))
 
-(define (prime? n)
-  (define (iter ps)
-    (cond ((> (square (stream-car ps)) n) #t)
-          ((divisible? n (stream-car ps)) #f)
-          (else (iter (stream-cdr ps)))))
-  (iter primes))
 
-(define primes
-  (stream-cons
-    2
-    (stream-filter prime? (integers-starting-from 3)))) 
+(print (p23))
 
-(define factorized-ht (make-hash-table))
-(define (print-hash-of-hash ht)
-  (for-each (lambda (k) 
-              (format #t "~s: " k) 
-              (print-hash-table2 (hash-table-get ht k))
-              (format #t "\n"))
-            (sort (hash-table-keys ht))))
-(define (print-hash-table2 ht)
-  (for-each (lambda (k) 
-              (format #t "(~s ~s) " k (hash-table-get ht k)))
-            (sort (hash-table-keys ht))))
-
-(define (copy-hash-table dst src)
-  (hash-table-for-each src (lambda (k v) (hash-table-put! dst k v)))
-  dst)
-
-(define (prime-factorize num)
-  (if (hash-table-get factorized-ht num #f)
-    (hash-table-get factorized-ht num)
-    (let1 ht (make-hash-table)
-          (let loop ((n num)
-                     (p primes))
-            (if (= n 1) 
-              (begin
-                (hash-table-put! factorized-ht num ht)
-                ht)
-              (let* ((prime (stream-car p))
-                     (div? (divisible? n prime))
-                     (q (quotient n prime)))
-                (when div? 
-                  (let ((qhash (hash-table-get factorized-ht q #f)))
-                    (when qhash
-                      (copy-hash-table ht qhash)
-                      (set! q 1))
-                    (hash-table-put! ht prime (+ (hash-table-get ht prime 0) 1)))) 
-                (loop (if div? q n) 
-                      (if div? p (stream-cdr p)))))))))
-
-(define (num-of-divisor num)
-  (hash-table-fold (prime-factorize num) 
-                   (lambda (k v p) (* p (+ v 1)))
-                   1))
-
-(let loop ((i 2))
-  (when (< (num-of-divisor i) 5)
-    ;(hash-table-put! factorized-ht i (prime-factorize i))
-    (format #t "~s ~s\n" i (num-of-divisor i))
-    (loop (+ i 1))))
-
-(print-hash-of-hash factorized-ht)
--- scm/util.scm
+++ scm/util.scm
@@ -1,1 +1,14 @@
 (define % (lambda (x y) (= (modulo x y) 0)))
+
+(define (divisors x)
+  (let ((limit (sqrt x)))
+    (let loop ((i 2) (result '(1)))
+      (if (> i limit)
+        result
+        (let* ((qm (call-with-values (lambda () (quotient&remainder x i)) cons))
+               (q (car qm))
+               (m (cdr qm)))
+          (loop (+ i 1)
+                (cond ((not (= m 0)) result)
+                      ((= q i) (cons i result))
+                      (else (list* q i result)))))))))

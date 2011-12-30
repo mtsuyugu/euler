5a10573b924c17149411a8acd5db957b64b68466
Masaaki Tsuyuguchi <mtsuyugu@gmail.com>
2011-12-30 10:46:56 +0900

    rewrite p37.scm

--- scm/p37.scm
+++ scm/p37.scm
@@ -1,30 +1,31 @@
 (use srfi-1)
-(load "./prime.scm")
+(load "./eratosthenes.scm")
 
 (define (keta n)
   (let loop ((n n) (i 0))
     (if (= n 0) i
       (loop (quotient n 10) (+ i 1)))))
 
-(define (truncate-l-r n)
-  (let1 k (keta n)
-        (map (lambda (k) (modulo n (expt 10 k))) (iota k 1))))
+(define (truncate-l-r? n keta-list)
+  (every (lambda (k) (prime? (modulo n (expt 10 k)))) keta-list))
 
-(define (truncate-r-l n)
-  (let1 k (keta n)
-        (map (lambda (k) (quotient n (expt 10 (- k 1)))) (iota k 1))))
+(define (truncate-r-l? n keta-list)
+  (every (lambda (k) (prime? (quotient n (expt 10 (- k 1))))) keta-list))
 
 (define (truncatable? n)
-  (and (every prime? (truncate-l-r n))
-       (every prime? (truncate-r-l n))))
+  (let1 keta-list (iota (keta n) 1)
+        (and (truncate-l-r? n keta-list)
+             (truncate-r-l? n keta-list))))
 
 (define (p37)
+  (eratosthenes 1000000)
   (let loop ((i 11) (r '()))
-    (format #t "~s ~s\n" i r)
     (if (= (length r) 11)
       (apply + r)
-      (loop (+ i 2) (if (truncatable? i)
+      (loop (+ i 2) (if (and (prime? i) (truncatable? i))
                       (cons i r)
                       r)))))
 
-
+(time
+(print (p37))
+)

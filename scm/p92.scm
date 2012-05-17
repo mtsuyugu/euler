d527a0c3aafae8561d89fa99dd38b4704a76e6f0
Masaaki Tsuyuguchi <mtsuyugu@gmail.com>
2012-05-17 23:28:53 +0900

    solved p92

--- scm/p92.scm
+++ scm/p92.scm
@@ -1,7 +1,36 @@
+(use srfi-1)
 
-(define square (lambda (x) (* x x)))
-(define (next n)
-  (+ (square (quotient n 10))
-     (square (modulo n 10))))
+(define (p92)
+  (letrec ((maxN 9999999)
+           (chain (lambda (n) 
+                    (letrec ((square (^x (* x x)))
+                             (iter (lambda (x sum)
+                                     (if (= x 0)
+                                       sum
+                                       (iter (quotient x 10) (+ (square (modulo x 10)) sum))))))
+                      (iter n 0))))
+           (check (lambda (n)
+                    (cond ((= n 89) #t)
+                          ((= n 1) #f)
+                          (else (check (chain n))))))
+           (table (make-hash-table))
+           (preliminary (lambda (n)
+                          (let1 maxChain (chain maxN)
+                                (if (<= n maxChain)
+                                  (begin
+                                    (hash-table-put! table n (check n))
+                                    (preliminary (+ n 1)))))))
+           (iter (lambda (n count)
+                   (if (> n maxN)
+                     count
+                     (iter (+ n 1)
+                           (+ count (if (hash-table-get table (chain n)) 1 0)))))))
+    (preliminary 1)
+    (iter 1 0)))
+   
+   
+(time  
+   (print (p92)) 
+)
 
-(define tbl (make-hash-table))
+

6e0cc62b0b731ca22f2862b0d2595e84c9fcc0ec
Masaaki Tsuyuguchi <mtsuyugu@gmail.com>
2012-10-27 09:14:02 +0900

    solved p65

---
+++ scm/p65.scm
@@ -0,0 +1,25 @@
+
+(define (p65)
+  (letrec ((denomi (lambda (n)
+                     (if (= (modulo n 3) 2) 
+                       (* 2 (+ (quotient n 3) 1))
+                       1)))
+           (nth-fract (lambda (n)
+                        (letrec ((iter (lambda (start end)
+                                         (if (= start end)
+                                           (/ 1 (denomi start))
+                                           (/ 1 (+ (denomi start) 
+                                                   (iter (+ start 1) end)))))))
+                          (if (= n 1) 2
+                            (+ 2 (iter 1 (- n 1)))))))
+           (sum-digits (lambda (num)
+                         (letrec ((iter (lambda (r n)
+                                          (if (= n 0)
+                                            r
+                                            (iter (+ r (modulo n 10))
+                                                  (quotient n 10))))))
+                           (iter 0 num)))))
+    (sum-digits (numerator (nth-fract 100)))))
+
+(print (p65))
+
